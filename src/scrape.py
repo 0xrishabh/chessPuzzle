b55632837efc4d0aa31d8159c6f87395498d2df1
cYph3r1337 <rishabhcodershukla@gmail.com>
2021-08-21 22:12:01 +0530

    🔥

---
+++ src/scrape.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from concurrent.futures import ThreadPoolExecutor
+import concurrent
+import chess
+
+http = requests.session()
+http.headers.update({"Content-Type": "application/json"})
+data = json.dumps({"type": "rated"})
+
+def scrape(http,data):
+	res = http.post("https://chessblunders.org/api/blunder/get", data=data)
+	puzzle_json = res.json()["data"]
+	fen_before = puzzle_json["fenBefore"]
+	board = chess.Board(fen_before)
+	blunder_move = puzzle_json["blunderMove"]
+	board.push_san(blunder_move)
+	fen_after = board.fen()
+	elo = puzzle_json["elo"]
+
+	forced_line = puzzle_json["forcedLine"]
+	print("{}|{}|{}".format(fen_after,forced_line,elo))
+	return True
+
+
+with ThreadPoolExecutor(max_workers=40) as executor:
+    processes = [executor.submit(scrape, http, data) for i in range(10)]
+    for future in concurrent.futures.as_completed(processes):
+    	future.result()
+
+
